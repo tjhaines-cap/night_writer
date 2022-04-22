7076364e7ddd4b6fe37e51dec35574c22703fed0
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-22 10:48:20 -0600

    Feat: test night writer exists

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,2 +1,14 @@
 require 'simplecov'
 SimpleCov.start
+require 'rspec'
+require './lib/night_writer'
+
+
+describe NightWriter do
+
+  it 'exists' do
+    night_writer = NightWriter.new
+    expect(night_writer).to be_a(NightWriter)
+  end
+
+end
