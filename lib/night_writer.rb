e408d938e825c330f0380d40d7b0520111c32f42
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-22 10:54:58 -0600

    Feat: output predefined message

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -3,7 +3,15 @@
 class NightWriter
 
   def initialize()
-
+    @braille_filename = "braille.txt"
+    @num_characters = 256
+    puts message
   end
-  
+
+  def message
+    message = "Created \'#{@braille_filename}\' containing #{@num_characters} characters"
+  end
+
 end
+
+night_writer = NightWriter.new
