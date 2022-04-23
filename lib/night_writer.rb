9ecfd030caa2ab63abe2cdb0616fc6cc2209d2b2
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-23 11:18:13 -0600

    Feat: count characters in given file

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,10 +1,11 @@
-
+require 'pry'
 
 class NightWriter
 
-  def initialize(mes, br)
-    @braille_filename = br
-    @num_characters = 256
+  def initialize(message_file, braille_file)
+    @message_filename = message_file
+    @braille_filename = braille_file
+    @num_characters = num_characters
     puts message
   end
 
@@ -12,6 +13,13 @@ class NightWriter
     message = "Created \'#{@braille_filename}\' containing #{@num_characters} characters"
   end
 
+  def num_characters
+    message_file = File.open(@message_filename, "r")
+    str = message_file.read.chomp
+    # binding.pry
+    return str.length
+  end
+
 end
 
-night_writer = NightWriter.new(ARGV[0], ARGV[1])
+# night_writer = NightWriter.new(ARGV[0], ARGV[1])
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -7,20 +7,15 @@ require './lib/night_writer'
 describe NightWriter do
 
   it 'exists' do
-    night_writer = NightWriter.new("message.txt", "braille.txt")
+    night_writer = NightWriter.new("test.txt", "braille.txt")
     expect(night_writer).to be_a(NightWriter)
   end
 
   it 'constructs message to output from given arguments' do
-    night_writer = NightWriter.new("message.txt", "braille.txt")
-    expect(night_writer.message).to eq("Created 'braille.txt' containing 256 characters")
-    night_writer = NightWriter.new("randon.txt", "other.txt")
-    expect(night_writer.message).to eq("Created 'other.txt' containing 256 characters")
-  end
-
-  it 'can count number of characters from first file given as argument' do
     night_writer = NightWriter.new("test.txt", "braille.txt")
     expect(night_writer.message).to eq("Created 'braille.txt' containing 15 characters")
+    night_writer = NightWriter.new("test.txt", "other.txt")
+    expect(night_writer.message).to eq("Created 'other.txt' containing 15 characters")
   end
 
 end
