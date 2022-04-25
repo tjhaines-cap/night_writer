70c462652a6f2b9dbaa7f048fcbbdc6df661ea4c
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-25 11:38:17 -0600

    Feat: reorganize text converter

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,8 @@
 require 'pry'
+require_relative 'text_converter'
 
-# night_writer = NightWriter.new(ARGV[0], ARGV[1])
-message_file = File.open(ARGV[0], "r")
-message_str = message_file.read.chomp
-num_chars = message_str.length
-puts "Created \'#{ARGV[1]}\' containing #{num_chars} characters"
+# message_file = File.open(ARGV[0], "r")
+# message_str = message_file.read.chomp
+text_converter = TextConverter.new(ARGV[0], ARGV[1])
+puts text_converter.message
+# puts "Created \'#{ARGV[1]}\' containing #{num_chars} characters"
--- lib/text_converter.rb
+++ lib/text_converter.rb
@@ -3,20 +3,17 @@ class TextConverter
   def initialize(message_file, braille_file)
     @message_filename = message_file
     @braille_filename = braille_file
-    @num_characters = num_characters
-    puts message
+    @message_file = File.open(@message_filename, "r")
+    @message_str = @message_file.read.chomp
+    @num_characters = message_length
   end
 
   def message
     message = "Created \'#{@braille_filename}\' containing #{@num_characters} characters"
   end
 
-  def num_characters
-    # binding.pry
-    message_file = File.open(@message_filename, "r")
-    str = message_file.read.chomp
-    # binding.pry
-    return str.length
+  def message_length
+    @message_str.length
   end
 
 end
