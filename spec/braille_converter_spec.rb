04b25ae12fc050a0d01bf50a72719a99e368b9b6
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-25 15:57:56 -0600

    Test: determine number of letters that will be in message

--- spec/braille_converter_spec.rb
+++ spec/braille_converter_spec.rb
@@ -11,6 +11,11 @@ describe BrailleConverter do
     expect(night_reader).to be_a(BrailleConverter)
   end
 
+  it 'can determine number of letters that will be in the message file' do
+    night_reader = BrailleConverter.new("braille.txt", "original_message.txt")
+    expect(night_reader.num_characters).to eq(11)
+  end
+
   it 'constructs message from arguments' do
     night_reader = BrailleConverter.new("braille.txt", "original_message.txt")
     expect(night_reader.message).to eq("Created 'original_message.txt' containing 11 characters")
